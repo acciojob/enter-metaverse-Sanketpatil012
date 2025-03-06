f81dc390092569a7feff1a414c5fd59e202d384b
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-06 10:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+
+let btn =  document.getElementById('enterBtn')
+let paragraph= document.getElementById('status')
+
+btn.addEventListener('click' , ()=>{
+	let create= document.createElement('h1')
+	 create.innerText="Entered Metaverse"
+	paragraph.replaceWith(create)
+})
+
+
+
